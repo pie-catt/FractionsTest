a6fc3a4c8858b6b3f413daad76d9c907d264937a
Pietro Cattaneo <job3_14@hotmail.it>
2017-11-20 18:41:50 +0100

    Primo commit

---
+++ FractionsTest.Tests/FractionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FractionsTest;
+using Moq;
+
+namespace FractionsTest.Tests
+{
+    [TestFixture]
+    public class FractionTests
+    {
+        [Test]
+        public void BaseConstructorTestNum()
+        {
+            var fr = new Mock<Fraction>(4, 5);
+            Assert.That(() => fr.Object.numerator == 4);
+        }
+
+        [Test]
+        public void BaseConstructorTestDen()
+        {
+            var fr = new Mock<Fraction>(4, 5);
+            Assert.That(() => fr.Object.denumerator == 5);
+        }
+
+        [Test]
+        public void BaseConstructorTestExcepion()
+        {
+           // var fr = new Mock<Fraction>(4, 0);
+            Assert.That(() => new Fraction(4, 0), Throws.TypeOf<ArgumentNullException>() );
+        }
+    }
+}
---
+++ FractionsTest/Fraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionsTest
+{
+    public class Fraction
+
+    {
+
+        public int numerator { get; }
+        public int denumerator { get; }
+
+        public Fraction(int n, int d)
+        {
+            if (d == 0)
+                throw new ArgumentNullException(nameof(d));
+            this.numerator = n;
+            this.denumerator = d;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
